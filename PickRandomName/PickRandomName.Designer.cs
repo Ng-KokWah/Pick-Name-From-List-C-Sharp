690b0fad1d270af889c696c0e6b135f1668306d8
Ng Kok Wah <kokwah98@gmail.com>
2017-09-06 20:24:13 +0800

    Add files via upload

---
+++ PickRandomName/PickRandomName.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PickRandomName
+{
+    partial class PickRandomName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnremove = new System.Windows.Forms.Button();
+            this.pick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(32, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(36, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(366, 196);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(111, 108);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(111, 41);
+            this.btnadd.TabIndex = 3;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 446);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Picked Name";
+            // 
+            // btnremove
+            // 
+            this.btnremove.Location = new System.Drawing.Point(272, 108);
+            this.btnremove.Name = "btnremove";
+            this.btnremove.Size = new System.Drawing.Size(111, 41);
+            this.btnremove.TabIndex = 6;
+            this.btnremove.Text = "Remove";
+            this.btnremove.UseVisualStyleBackColor = true;
+            this.btnremove.Click += new System.EventHandler(this.btnremove_Click);
+            // 
+            // pick
+            // 
+            this.pick.Location = new System.Drawing.Point(188, 387);
+            this.pick.Name = "pick";
+            this.pick.Size = new System.Drawing.Size(111, 41);
+            this.pick.TabIndex = 7;
+            this.pick.Text = "Pick Name";
+            this.pick.UseVisualStyleBackColor = true;
+            this.pick.Click += new System.EventHandler(this.pick_Click);
+            // 
+            // PickRandomName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 510);
+            this.Controls.Add(this.pick);
+            this.Controls.Add(this.btnremove);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PickRandomName";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.PickRandomName_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnremove;
+        private System.Windows.Forms.Button pick;
+    }
+}
+
---
+++ PickRandomName/PickRandomName.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PickRandomName
+{
+    public partial class PickRandomName : Form
+    {
+        public static String previousname;
+        public PickRandomName()
+        {
+            InitializeComponent();
+        }
+
+        private void PickRandomName_Load(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void btnremove_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                String temp = listBox1.SelectedItem.ToString();
+                listBox1.Items.Remove(temp);
+            }
+        }
+
+        private void pick_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            int size = listBox1.Items.Count;
+            textBox2.Text = listBox1.Items[ran.Next(0, size)].ToString();
+        }
+    }
+}
